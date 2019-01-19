7018398b06128542bf6886365ca5fe4327a8471a
Anatoliy <boldinao@icloud.com>
2019-01-19 23:35:39 +0500

    Выполнено ДЗ 2

---
+++ GB8/HW2/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace HW2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TbAnswer = new System.Windows.Forms.TextBox();
+            this.BtGiveAnswer = new System.Windows.Forms.Button();
+            this.LbQuestion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TbAnswer
+            // 
+            this.TbAnswer.Location = new System.Drawing.Point(99, 49);
+            this.TbAnswer.Name = "TbAnswer";
+            this.TbAnswer.Size = new System.Drawing.Size(263, 22);
+            this.TbAnswer.TabIndex = 0;
+            // 
+            // BtGiveAnswer
+            // 
+            this.BtGiveAnswer.Location = new System.Drawing.Point(142, 92);
+            this.BtGiveAnswer.Name = "BtGiveAnswer";
+            this.BtGiveAnswer.Size = new System.Drawing.Size(171, 23);
+            this.BtGiveAnswer.TabIndex = 1;
+            this.BtGiveAnswer.Text = "Ответить";
+            this.BtGiveAnswer.UseVisualStyleBackColor = true;
+            this.BtGiveAnswer.Click += new System.EventHandler(this.BtGiveAnswer_Click);
+            // 
+            // LbQuestion
+            // 
+            this.LbQuestion.AutoSize = true;
+            this.LbQuestion.Location = new System.Drawing.Point(96, 9);
+            this.LbQuestion.Name = "LbQuestion";
+            this.LbQuestion.Size = new System.Drawing.Size(46, 17);
+            this.LbQuestion.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 171);
+            this.Controls.Add(this.LbQuestion);
+            this.Controls.Add(this.BtGiveAnswer);
+            this.Controls.Add(this.TbAnswer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TbAnswer;
+        private System.Windows.Forms.Button BtGiveAnswer;
+        private System.Windows.Forms.Label LbQuestion;
+    }
+}
+
---
+++ GB8/HW2/TrueFalse.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CompareAnswere
+{
+    public class Questions
+    {
+        string question;
+        string trueFalse;
+
+        public string Question { get => question; set => question = value; }
+
+        public string TrueFalse { get => trueFalse; set => trueFalse = value; }
+
+        public Questions(string question, string trueFalse)
+        {
+            this.question = question;
+            this.trueFalse = trueFalse;
+        }
+
+        public Questions()
+        {
+
+        }
+    }
+
+    static class GetRandomQuestion
+    {
+        static Random random;
+        static List<Questions> qa;
+        static int numQuestion;
+
+        static public List<Questions> QA => qa;
+
+        static public int NumQuestion => numQuestion;
+
+        static public string GetQuestion()
+        {
+            random = new Random();
+            qa = new List<Questions>(File.Deserialize());
+            numQuestion = random.Next(0, qa.Count());
+            return qa[numQuestion].Question;
+        }
+    }
+
+    static class Compare
+    {
+        static string answer;
+
+        static string Answer { get => answer; set => answer = value; }
+
+        static public bool Answers(string myAnswer)
+        {
+            Answer = GetRandomQuestion.QA[GetRandomQuestion.NumQuestion].TrueFalse;
+            return (myAnswer == "+" && Answer == "true") || (myAnswer == "-" && Answer == "false") ? true : false;
+        }
+    }
+
+    static class File
+    {
+        static string fileName = "questions.xml";
+
+        static Random random = new Random();
+        
+
+        //static public void Serialize(List<Questions> questions)
+        //{
+        //    XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Questions>));
+        //    Stream streamReader = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+        //    xmlSerializer.Serialize(streamReader, questions);
+        //    streamReader.Close();
+        //}
+
+        static public List<Questions> Deserialize()
+        {
+            List<Questions> questions = new List<Questions>();
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Questions>));
+            Stream streamReader = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            questions = (List<Questions>)xmlSerializer.Deserialize(streamReader);
+            streamReader.Close();
+            return questions;
+        }
+    }
+}
